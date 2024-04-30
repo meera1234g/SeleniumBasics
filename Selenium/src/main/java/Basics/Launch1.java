5bf794b1bbb46bf2c6f3674b068e1eac26cbd209
Meera Rupak <Meera Rupak@192.168.1.104>
2024-04-30 22:49:56 +0530

    second commit

--- Selenium/src/main/java/Basics/Launch1.java
+++ Selenium/src/main/java/Basics/Launch1.java
@@ -1,10 +1,14 @@
 package Basics;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Launch1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		 WebDriver driver = new ChromeDriver();// Webdriver interface and Chrome Driver is a Class 
+		    driver.get("https://www.seleniumeasy.com/");
 	}
 
 }
